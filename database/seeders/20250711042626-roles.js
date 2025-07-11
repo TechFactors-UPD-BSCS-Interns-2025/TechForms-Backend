af092dcddb1660aaeffbb8f188747f986af1c3bc
Angel Hernandez <amhernandez@techfactors.com>
2025-07-11 12:51:27 +0800

    Add seeder for roles table

---
+++ database/seeders/20250711042626-roles.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const role = require('../../app/models/role');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('roles', [
+      {	
+        role_name: 'HR',
+        created_at: new Date(),
+        created_by: 1,
+        updated_at: new Date(),
+        updated_by: 1,
+      },
+      {	
+        role_name: 'Employee',
+        created_at: new Date(),
+        created_by: 1,
+        updated_at: new Date(),
+        updated_by: 1,
+      },
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('roles', null, {});
+  }
+};
