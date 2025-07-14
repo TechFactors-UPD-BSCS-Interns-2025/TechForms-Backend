a2bc7fe717763c226017a65a1fccd611d86bce8d
TATSmebro <marcus.pilapil@gmail.com>
2025-07-14 15:10:06 +0800

    merged routing changes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -5,11 +5,8 @@ const { RoleRoutes } = require("./role/role_routes");
 const { ApproverRoutes } = require("./approver/approver_routes");
 const { UserProfileRoutes } = require("./user_profile/user_profile_routes");
 const { FlierRoutes } = require("./flier/flier_routes");
-<<<<<<< HEAD
 const { PurposeOfTravelRoutes } = require("./purpose_of_travel/purpose_of_travel_routes");
-=======
 const { BookingDetailsRoutes } = require("./booking_details/booking_details_routes");
->>>>>>> main
 
 var router = express.Router();
 
@@ -19,10 +16,7 @@ router.use("/role", RoleRoutes);
 router.use("/approver", ApproverRoutes);
 router.use("/user_profile", UserProfileRoutes);
 router.use("/flier", FlierRoutes);
-<<<<<<< HEAD
 router.use("/purpose_of_travel", PurposeOfTravelRoutes);
-=======
 router.use("/booking_details", BookingDetailsRoutes);
->>>>>>> main
 
 module.exports = router;
