519cb605faa0c559d5e812b2d448da597cc5acf0
Angel Hernandez <amhernandez@techfactors.com>
2025-07-14 16:27:53 +0800

    Fix schema for Booking Details

--- app/schemas/booking_details/booking_details_schema.js
+++ app/schemas/booking_details/booking_details_schema.js
@@ -7,7 +7,17 @@
  *       properties:
  *         id:
  *           type: integer
- *         role_name:
+ *         departure_ref_no:
+ *           type: string
+ *         departure_cost:
+ *           type: number
+ *         departure_ticket_path:
+ *           type: string
+ *         return_ref_no:
+ *           type: string
+ *         return_cost:
+ *           type: number
+ *         return_ticket_path:
  *           type: string
  *     BookingDetailsGetInformationResponse:
  *       allOf:
