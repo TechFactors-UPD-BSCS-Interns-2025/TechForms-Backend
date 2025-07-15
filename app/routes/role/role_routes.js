9b2a2bd14f527f9ca92c93d27cdc0ae3c83c697c
Angel Hernandez <amhernandez@techfactors.com>
2025-07-15 16:01:06 +0800

    Add CUD to Role Table

--- app/routes/role/role_routes.js
+++ app/routes/role/role_routes.js
@@ -3,7 +3,31 @@ const router = express.Router()
 
 const { RoleController } = require('../../controllers/role/role_controller')
 
-// TODO: Update schema ref
+/**
+ *  @openapi
+ *  /api/v1/role/create:
+ *    post:
+ *      tags: 
+ *        - Role
+ *      description: Create Role API.
+ *      summary: Create New Role
+ *      security: 
+ *        - bearerAuth: []
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/RoleGetInformation'
+ *      responses:
+ *        200:
+ *          description: Ok
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/RoleGetInformationResponse'
+*/
+router.post('/create', RoleController.create)
 
 /**
  *  @openapi
@@ -50,5 +74,64 @@ router.get('/all', RoleController.all)
  *      
 */
 router.get('/:id', RoleController.get)
+/**
+ *  @openapi
+ *  /api/v1/role/{id}:
+ *    put:
+ *      tags: 
+ *        - Role
+ *      description: Update Specific Role by Id API.
+ *      summary: Update Specific Role
+ *      security: 
+ *        - bearerAuth: []
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *           type: integer
+ *          required: true
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/RoleGetInformation'
+ *      responses:
+ *        200:
+ *          description: Ok
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/RoleGetInformationResponse'
+ *      
+*/
+router.put('/:id', RoleController.update)
+/**
+ *  @openapi
+ *  /api/v1/role/{id}:
+ *    delete:
+ *      tags: 
+ *        - Role
+ *      description: Delete Specific Role by Id API.
+ *      summary: Delete Specific Role
+ *      security: 
+ *        - bearerAuth: []
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *           type: integer
+ *          required: true
+ *      responses:
+ *        200:
+ *          description: Ok
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/RoleGetInformationResponse'
+ *      
+*/
+router.delete('/:id', RoleController.delete)
+
 
 module.exports.RoleRoutes = router
