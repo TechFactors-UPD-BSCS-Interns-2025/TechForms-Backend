f7f006e67e0abfdaf4f6dffe2cb585cdf504f347
Angel Hernandez <amhernandez@techfactors.com>
2025-07-16 10:18:01 +0800

    Add CUD to User Credentials Table

--- app/routes/user_credentials/user_credentials_routes.js
+++ app/routes/user_credentials/user_credentials_routes.js
@@ -3,8 +3,31 @@ const router = express.Router()
 
 const { UserCredentialsController } = require('../../controllers/user_credentials/user_credentials_controller')
 
-// TODO: Update schema ref
-
+/**
+ *  @openapi
+ *  /api/v1/user_credentials/create:
+ *    post:
+ *      tags: 
+ *        - User Credentials
+ *      description: Create User Credentials API.
+ *      summary: Create New User Credentials
+ *      security: 
+ *        - bearerAuth: []
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/UserCredentialsGetInformation'
+ *      responses:
+ *        200:
+ *          description: Ok
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/UserCredentialsGetInformationResponse'
+*/
+router.post('/create', UserCredentialsController.create)
 /**
  *  @openapi
  *  /api/v1/user_credentials/all:
@@ -50,5 +73,63 @@ router.get('/all', UserCredentialsController.all)
  *      
 */
 router.get('/:id', UserCredentialsController.get)
+/**
+ *  @openapi
+ *  /api/v1/user_credentials/{id}:
+ *    put:
+ *      tags: 
+ *        - User Credentials
+ *      description: Update Specific User Credentials by Id API.
+ *      summary: Update Specific User Credentials
+ *      security: 
+ *        - bearerAuth: []
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *           type: integer
+ *          required: true
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/UserCredentialsGetInformation'
+ *      responses:
+ *        200:
+ *          description: Ok
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/UserCredentialsGetInformationResponse'
+ *      
+*/
+router.put('/:id', UserCredentialsController.update)
+/**
+ *  @openapi
+ *  /api/v1/user_credentials/{id}:
+ *    delete:
+ *      tags: 
+ *        - User Credentials
+ *      description: Delete Specific User Credentials by Id API.
+ *      summary: Delete Specific User Credentials
+ *      security: 
+ *        - bearerAuth: []
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *           type: integer
+ *          required: true
+ *      responses:
+ *        200:
+ *          description: Ok
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/UserCredentialsGetInformationResponse'
+ *      
+*/
+router.delete('/:id', UserCredentialsController.delete)
 
 module.exports.UserCredentialsRoutes = router
