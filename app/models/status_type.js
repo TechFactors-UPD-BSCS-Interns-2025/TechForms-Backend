25642d5a050bac59210e95cfa36d16024cf5676c
TATSmebro <marcus.pilapil@gmail.com>
2025-07-21 10:04:48 +0800

    updated status type association type

--- app/models/status_type.js
+++ app/models/status_type.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      StatusType.hasOne(models.Request, {
+      StatusType.hasMany(models.Request, {
         foreignKey: 'status_id',
       });
     }
